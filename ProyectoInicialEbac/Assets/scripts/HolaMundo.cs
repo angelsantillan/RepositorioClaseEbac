f63e5a5eeceea9b04dfe23fd8fef8c688844cb8d
angelsantillan <angel.santillan@gmail.com>
2025-03-30 23:56:39 -0600

    added script

---
+++ ProyectoInicialEbac/Assets/scripts/HolaMundo.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Algo pasa");
+        Debug.Log("Hola Mundo");
+        Debug.LogWarning("Algo esta raro");
+        Debug.LogError("Algo trono");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
